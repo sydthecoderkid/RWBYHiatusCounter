b507f3fdea60ea4b55ea991d19431733560d8a10
sydthecoderkid <bookmasterdc@gmail.com>
2020-09-20 23:59:01 -0400

    Updating

--- Code/OpenStream.py
+++ Code/OpenStream.py
@@ -3,18 +3,28 @@ import requests
 import json
 import pytumblr
 from datetime import datetime
-
+import datetime
+from datetime import date
 
 def connectToAPI():
    client = pytumblr.TumblrRestClient('04jDlKiLcnVV6hYsLtPiBz198r8Y4Uc8HPGQxhIe9GXfW1gC8i')
    callAPI(client)
-   
- 
+  
    
 
 def callAPI(obj):
-   print("HERE")
-   print(obj.blog_info('sydthecoderkid.tumblr.com'))
+    bloginfo = obj.blog_info('rwbyhiatuscountdown.tumblr.com')
+    title = "Blog was last updated:"
+    print(title, datetime.datetime.fromtimestamp(bloginfo['blog']['updated']))
+    print("Today is:", date.today())
+    print(bloginfo['blog']['title'])
+ 
+     
+          
 
 
+
+
+
+connectToAPI()
     
